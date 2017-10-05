cf55d1da303ff30518415ac27e39b9169a64dafa
Weston Ruter <weston@xwp.co>
2017-10-04 20:51:36 -0700

    Add link to Custom CSS section in Customizer

--- src/wp-admin/theme-editor.php
+++ src/wp-admin/theme-editor.php
@@ -318,7 +318,15 @@ if ( ! in_array( 'theme_editor_notice', $dismissed_pointers, true ) ) :
 	<div class="notification-dialog" role="dialog" aria-labelledby="file-editor-warning-title" tabindex="0">
 		<div class="file-editor-warning-content">
 			<h1 id="file-editor-warning-title"><?php _e( 'Heads up!' ); ?></h1>
-			<p><?php _e( 'You appear to be making direct edits to your theme in the WordPress dashboard. We recommend that you don&#8217;t! Editing this code directly is dangerous, and can leave you unable to log back in to WordPress and undo changes. There’s no need to change your CSS here &mdash; you can edit and live preview CSS changes in WordPress&#8217;s built in CSS editor.' ); ?></p>
+			<p>
+				<?php
+				echo sprintf(
+					/* translators: %s is link to Custom CSS section in the Customizer. */
+					__( 'You appear to be making direct edits to your theme in the WordPress dashboard. We recommend that you don&#8217;t! Editing this code directly is dangerous, and can leave you unable to log back in to WordPress and undo changes. There’s no need to change your CSS here &mdash; you can edit and live preview CSS changes in WordPress&#8217;s <a href="%s">built in CSS editor</a>.' ),
+					esc_url( add_query_arg( 'autofocus[section]', 'custom_css', admin_url( 'customize.php' ) ) )
+				);
+				?>
+			</p>
 			<p><?php _e( 'If you decide to go ahead with direct edits anyway, make sure to back up all your site&#8217;s files before making changes so you can restore a functional version if something goes wrong. Once dismissed, this notice will not appear again.' ); ?></p>
 		</div>
 		<button type="button" class="notice-dismiss"><span class="screen-reader-text"><?php _e( 'Dismiss' ); ?></span></button>
